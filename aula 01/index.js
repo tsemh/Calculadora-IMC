99b702d0a3e6c752df571c54f8b000edfab73c9d
tsemh <tiago_barbosa-silva@hotmail.com>
2021-10-25 17:35:56 -0300

    teste

---
+++ aula 01/index.js
@@ -0,0 +1,34 @@
+/* OPERADORES DE COMPARAÇÃO
+*
+* " > " Maior que
+* " >= " Maior OU igual
+* " < " Menor que
+* " <=  " Menos OU igual
+* " == " Igualdade (valor)  -> NÂO RECOMENDADO
+* " === " Igualdade estrita ( valor e tipo)
+* " != " Diferente (valor)  -> NÂO RECOMENDADO
+* " !== " Diferente estrito (valor e tipo)
+*
+*/
+
+/* OPORADORES LÓGICOS
+*
+* && -> AND -> E
+* || -> OR  -> ou
+* !  -> NOT -> NÂO
+*
+*/
+
+/* if, else if, else
+*
+*   if pode ser usado sozinho 
+*   Else só pode ser utilizado caso aja um " if " antes e
+pode existir apenas um por condição no final desta.
+*   Else if pode ser criado varias vezes
+*
+*/
+const hora = 12;
+
+if (hora > 0 && hora < 13) {
+    console.log('');
+}
